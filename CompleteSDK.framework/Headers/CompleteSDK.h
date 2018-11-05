202ae62b6babab35be0ac2926e771de7ced84d63
AcrelecGitHub <AcrelecGitHub@acrelec.com>
2018-11-04 20:44:45 -0500

    Initial Commit

---
+++ CompleteSDK.framework/Headers/CompleteSDK.h
@@ -0,0 +1,19 @@
+//
+//  CompleteSDK.h
+//  CompleteSDK
+//
+//  Created by Adam Schuster on 10/16/18.
+//  Copyright © 2018 Acrelec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CompleteSDK.
+FOUNDATION_EXPORT double CompleteSDKVersionNumber;
+
+//! Project version string for CompleteSDK.
+FOUNDATION_EXPORT const unsigned char CompleteSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CompleteSDK/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # complete-ios-sdk
 This repository will host the complete iOS sdk dependency
+
+## Installation
+
+### CocoaPods
+
+Coming soon.
+
+### Manually
+
+1. Download the full project as a ZIP from here.
+2. Add the CompleteSDK.framework to your Xcode project as embeded binary.
+
+##Usage
+
+[Click here for documentation](https://acrelec.gitbook.io/complete/getting-started/ios-app-quick-start)
